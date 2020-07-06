8c209e11a49ce283b57bd08be4228741cf6e9e0a
Grigory Semakin <grinsdot1@gmail.com>
2020-07-06 08:32:19 +0300

    services start

---
+++ components/services/links.service.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-undef */
+const BASE_URL = '';
+
+/** 
+ * Data source for links collection
+*/
+export class LinksService {
+    /** 
+     * Wrapper for XMLHttpRequest
+     * @param {string} type
+     * @param {string} url
+     * @param {Object} data
+     * @param {Function} callback
+    */
+
+    static _makeRequest(type, url, data, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open(type, url, true);
+
+        xhr.addEventListener("readystatechange", () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status !== 200) {
+                    console.error('Сетевая ошибка', xhr);
+                } else {
+                    callback(JSON.parse(xhr.responseText));
+                }
+    
+            }
+           
+        });
+
+        if (typeof data !== 'undefined') {
+            data = JSON.stringify(data);
+        }
+
+        xhr.send(data);
+    }
+
+
+
+  
+
+    /** 
+     * Get collection
+     * @param {Function} callback
+    */
+    static getLinks(callback) {
+        this._makeRequest('GET', BASE_URL, undefined, callback);
+    }
+
+    /** 
+     * Update collections
+     * @param {Object} links
+     * @param {Function} callback
+    */
+    static putLinks(links, callback) {
+        this._makeRequest('PUT', BASE_URL, links, callback);
+    }
+}
